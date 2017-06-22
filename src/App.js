dc1eb2af85c2eafad003bff6a9bfa687b241b7a6
Sandra <sandra.dudley@ymail.com>
2017-06-22 07:16:24 +0100

    Added datepicker npm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Events from './components/Events';
+import ChooseDate from './components/ChooseDate';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ChooseDate />
         <Events />
       </div>
     );
