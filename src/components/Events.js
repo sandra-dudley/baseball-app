321c797966532194581094af956a0927d98ec2d8
Sandra <sandra.dudley@ymail.com>
2017-06-22 05:48:38 +0100

    Added Events component
    This is where the result events will appear

---
+++ src/components/Events.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Events extends Component {
+  render () {
+    return(
+      <p>Display events here</p>
+      )
+  }
+}
+
+export default Events;
