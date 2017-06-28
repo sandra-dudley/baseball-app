617b10d3fc6d160798708288f3a7bba319739d82
Sandra Dudley <sandra.dudley@ymail.com>
2017-06-28 06:45:58 +0000

    Excluded days before today

--- src/components/ChooseDate.js
+++ src/components/ChooseDate.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 import styles from './ChooseDate.css';
 import moment from 'moment';
  
