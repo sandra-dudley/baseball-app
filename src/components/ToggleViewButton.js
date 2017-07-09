b658b8caf89c9a176cfdd30a4ad4ed6be6a983f6
Sandra Dudley <sandra.dudley@ymail.com>
2017-07-09 18:03:15 +0000

    Styled toggle buttons

--- src/components/ToggleViewButton.js
+++ src/components/ToggleViewButton.js
@@ -10,13 +10,13 @@ class ToggleViewButton extends Component {
           <div className="btn-group toggleView">
             <label className={
               (this.props.mapView) ? "btn btn-primary" : "btn btn-primary active"
-            }>
-              <input type="radio"value="false" onChange={this.props.mapVisibility} checked={this.props.mapView === false} /> View list
+            } id="listButton">
+              <input type="radio"value="false" onChange={this.props.mapVisibility} checked={this.props.mapView === false} /> View events in a list
             </label>
             <label className={
               (this.props.mapView) ? "btn btn-primary active" : "btn btn-primary"
-            }>
-              <input type="radio" value="true" onChange={this.props.mapVisibility} checked={this.props.mapView === true}/> View map
+            } id="mapButton">
+              <input type="radio" value="true" onChange={this.props.mapVisibility} checked={this.props.mapView === true}/> View events on a map
             </label>
           </div>
         </div>
